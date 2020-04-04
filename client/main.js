bca0ebfa5a386fbb4bd8dffc7731f571de044362
Brien1303 <aleo2002@hotmail.com>
2020-04-04 14:09:52 -0400

    trying to add images

--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import 'meteor/jkuester:blaze-bs4'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // this is the default BS theme as example
+import popper from 'popper.js'
+global.Popper = popper // fixes some issues with Popper and Meteor
 import './main.html';
+import '../lib/collection.js';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.allBooks.helpers({
+	allbooks(){
+		return booksdb.find();
+	},
+})
+
+/*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
@@ -19,4 +30,27 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
+});*/
+
+Template.myJumbo.events({
+	'click .js-save' (event, instance){
+		var bookName = $('#Title').val();
+		var bookLink = $('#bookURL').val();
+		var Des = $('#Description').val();
+		booksdb.insert({
+			"Title" : bookName,
+			"bookURL" : bookLink,
+			"Description": Des
+		});
+		console.log ("saving...");
+				$("#addImageModal").modal("hide");
+				$("#Title").val("");
+				$("#bookURL").val("");
+				$("#Description").val("");
+
+	},
+	'input #bookURL'(event, instance){
+			$(".mountains").attr ("src", $ ("#bookURL") .val());
+			console.log($("#bookURL").val());
+		}
 });
