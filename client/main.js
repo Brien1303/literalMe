0a77516025491df710e30fea6a47a46aada01d65
Brien1303 <aleo2002@hotmail.com>
2020-04-06 19:14:47 -0400

    veiw button

--- client/main.js
+++ client/main.js
@@ -8,11 +8,11 @@ global.Popper = popper // fixes some issues with Popper and Meteor
 import './main.html';
 import '../lib/collection.js';
 
-Template.allBooks.helpers({
-	allbooks(){
+Template.mainBody.helpers({
+	allBooks(){
 		return booksdb.find();
 	},
-})
+});
 
 /*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -42,7 +42,7 @@ Template.myJumbo.events({
 			"bookURL" : bookLink,
 			"Description": Des
 		});
-		console.log ("saving...");
+		console.log ("saving..." + "Title:" + bookName + "URL:"+ bookLink + "Description:"+ Des);
 				$("#addImageModal").modal("hide");
 				$("#Title").val("");
 				$("#bookURL").val("");
@@ -53,4 +53,41 @@ Template.myJumbo.events({
 			$(".mountains").attr ("src", $ ("#bookURL") .val());
 			console.log($("#bookURL").val());
 		}
+
 });
+
+Template.veiwBook.events({
+'click .js.delete' (event, instance){
+	var myId = $ ('#veiwId').val();
+	$("#deleteId").val(myId);
+	$('#confirmModal').modal('show');
+},
+'click .js-confirm'(event, instance){
+	var myId = $('#deleteId').val();
+	$("#"+myId).fadeOut('slow',function(){
+		booksdb.remove({_id:myId});
+	});
+},
+
+	
+});
+
+Template.mainBody.events({
+	'click .js-veiwBook'(event, instance){
+		$("#veiwBookModal").modal("show");
+		var myId = this._id;
+		var theTitle = booksdb.findOne({_id:myId}).title;
+		var thePath = booksdb.findOne({_id:myId}).path;
+		var theDes = booksdb.findOne({_id:myId}).des;
+		var theAuthor = booksdb.findOne({_id:myId}).author;
+		$("#veiwId").val(myId);
+		$("#veiwTitle").val(myTitle);
+		$("#veiwDes").val(myDes);
+		$("#veiwAuthor").val(myAuthor);
+		$("#veiwHolder").attrc("src", thePath);
+
+		var myId = this._id;
+		instance.veiws.set(instance).set(instance.veiws.get(myId) + 1);
+	},
+
+})
